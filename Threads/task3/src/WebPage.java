d31cf1903b340d4d75a51a5d2dc0549eac1ccca8
himynameisoleg <perchykoleg@gmail.com>
2021-03-26 18:56:10 -0500

    Finished Task 3, passing

--- Threads/task3/src/WebPage.java
+++ Threads/task3/src/WebPage.java
@@ -1,31 +1,42 @@
 /* TODO
 Imports will be filling up as you need them. */
 
+import java.io.FileWriter;
+import java.util.LinkedList;
+import java.util.List;
+
 public class WebPage {
     public List<String> dom; // this actually has the html document model.
 
     public WebPage(){
-        dom = /* TODO 
-Initialize this to some kind of List */;
+        dom = new LinkedList<>();
     }
 
     public void addTitle(String title){
-        /* TODO 
-Add a title to the DOM*/
+        dom.add("<h1>" + title + "</h1>");
     }
 
-    /* TODO 
-implement addParagraph that receives a String and puts it in the dom 
-surrounded by <p> and </p>*/
+    public void addParagraph(String para) {
+        dom.add("<p>" + para + "</p>");
+    }
     public String toString(){
         String html = "<html>\n<head>\n</head>\n<body>";
-        /* TODO
-Loop through the elemments of the dom and append them to html */
+
+        for (String el : dom) {
+            html += el;
+        }
+
         return html+"</body>\n</html>";
     }
 
     public void save(String filename){
-        /* TODO 
-Save a string representation of this webpage to the filename provided.*/
+
+        try {
+            FileWriter fw = new FileWriter(filename);
+            fw.write(toString());
+            fw.close();
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
     }
 }
--- Threads/task3/src/WebPageEditor.java
+++ Threads/task3/src/WebPageEditor.java
@@ -1,36 +1,55 @@
 /* TODO 
 Imports. Fill them up as you need them.*/
 
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class WebPageEditor {
     public  String temp; // holds the last webpage saved.
     public  WebPage wp; // the webpage you are working on
     public  String filename; // the filename to save this webpage
 
     public WebPageEditor(String filename){
-/* TODO
-Initialize wp to an empty webpage, and filename to the value passed. */
+        wp = new WebPage();
+        this.filename = filename;
     }
 
     public void start(){
 
-        ScheduledExecutorService service = /* TODO
-Using Executors, instantiate a new SingleThreadedScheduledExecutor */
-        /* TODO 
-Schedule at a fixed rate a runnable that executes checkSave. if checkSave is true it will print "Saved", This has to be schdule to start in 10 seconds and repeat every 10 seconds.  */
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+        service.scheduleAtFixedRate(()->{checkSave();}, 10, 10, TimeUnit.SECONDS);
 
         // use a scanner to read every line the user writes
         Scanner kbd  = new Scanner(System.in);
         boolean finish = false;
-        while(!finish){
-/* TODO 
-If the user types "quit" the loop ends. If the input starts with t: you add a title to the webpage. If not, you add a paragraph.*/
+        while(!finish) {
+            String in = kbd.nextLine();
+            if (in.equals("quit")) {
+                finish = true;
+            } else if (in.contains("t:")) {
+                wp.addTitle(in);
+            } else {
+               wp.addParagraph(in);
+            }
         }
-        service./* TODO 
-Shutdown the threads NOW*/; // when the loop finishes, threads are stopped
+
+        service.shutdownNow();
     }
 
     public  boolean checkSave(){
-        /* TODO 
-If the text of the wp (String representation) is equal to the last String reperesentation (stored in tmp) returns false. Otherwise, it saves the webpage to the given filenam, tmp updates to the most current string representation of this webpage, and the funtion returns true.*/
+        if (!wp.toString().equals(temp)) {
+            this.temp = wp.toString();
+            wp.save(filename);
+            return true;
+        } else {
+            return false;
+        }
+        /* TODO
+If the text of the wp (String representation) is equal to the last String reperesentation
+(stored in tmp) returns false. Otherwise, it saves the webpage to the given filenam,
+tmp updates to the most current string representation of this webpage, and the funtion returns true.*/
     }
 }
