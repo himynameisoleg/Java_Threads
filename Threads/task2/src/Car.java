f50412fd1069c91901f45474114f99a7f3902eed
himynameisoleg <perchykoleg@gmail.com>
2021-03-26 16:30:07 -0500

    Completed Task2 all passing

--- Threads/task2/src/Car.java
+++ Threads/task2/src/Car.java
@@ -4,6 +4,23 @@ public class Car {
     public String name;
     public double maxMiles;
 
- /* TODO 
-Exact same methods and constructor as the Car in task 1*/
+    public Car(String name,double miles){
+        this.miles = miles;
+        odometer = 0;
+        this.name = name;
+        this.maxMiles = miles;
+    }
+
+    public void move(double miles){
+        this.miles -= miles;
+        this.odometer += miles;
+    }
+
+    public void charge(){
+        this.miles = maxMiles;
+    }
+
+    public String toString(){
+        return name+": odo:"+odometer+" - miles left:"+miles;
+    }
 }
--- Threads/task2/src/Racer.java
+++ Threads/task2/src/Racer.java
@@ -1,3 +1,27 @@
-public class Racer /* TODO 
-This class is exactly as the Racer from Task 1*/
+public class Racer implements Runnable {
+    Car c;
+    int distance = 1000; // this is a racer that is going to run 1000 miles.
+
+
+    public Racer(Car c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (this.c.odometer <= distance) {
+                if (this.c.miles == 0) {
+                    long rechargeTime = (long) this.c.maxMiles;
+                    Thread.sleep(rechargeTime);
+                    this.c.miles = this.c.maxMiles;
+                }
+                this.c.move(1);
+
+            }
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Hello " + this.c.name);
+    }
 }
